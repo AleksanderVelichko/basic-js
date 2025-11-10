a1fb765c49068255c81b2e4f2062e4ecfd545593
Velichko Alexander <sasha0992@gmail.com>
2025-11-11 02:46:11 +0400

    fix: resolve delete digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require('../lib')
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,16 @@ const { NotImplementedError } = require('../lib');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  let str = n.toString()
+  let arr = []
+
+  for (let i = 0; i < str.length; i++) {
+    arr.push(Number(str.slice(0, i) + str.slice(i + 1)))
+  }
+  return Math.max(...arr)
 }
 
 module.exports = {
-  deleteDigit
-};
+  deleteDigit,
+}
