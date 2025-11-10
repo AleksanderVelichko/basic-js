6727ca6105bfd82c80be9b6a7e9c35604965af76
Velichko Alexander <sasha0992@gmail.com>
2025-11-11 01:16:50 +0400

    fix: resolve extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require('../lib')
 
 /**
  * Create a repeating string based on the given parameters
@@ -16,11 +16,31 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1
+  let separator = options.separator || '+'
+  let addition = options.addition
+  let additionRepeatTimes = options.additionRepeatTimes || 1
+  let additionSeparator = options.additionSeparator || '|'
+
+  let resStr = []
+
+  for (let i = 0; i < repeatTimes; i++) {
+    let mainStr = str
+    if (addition !== undefined) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        if (j + 1 < additionRepeatTimes) {
+          mainStr += addition + additionSeparator
+        } else if (j + 1 == additionRepeatTimes) {
+          mainStr += addition + ''
+        }
+      }
+    }
+    resStr.push(mainStr)
+  }
+  return resStr.join(separator + '')
 }
 
 module.exports = {
-  repeater
-};
+  repeater,
+}
