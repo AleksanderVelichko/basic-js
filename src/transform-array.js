b2f7371ee79c979d4b071027b6cbf20473f9a1d8
Velichko Alexander <sasha0992@gmail.com>
2025-11-11 00:43:47 +0400

    fix: resolve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require('../lib')
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,28 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  const transArray = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') i++
+    else if (arr[i] == '--discard-prev') {
+      if (transArray[transArray.length - 1] == arr[i - 1]) transArray.pop()
+    } else if (arr[i] == '--double-next') {
+      if (arr[i + 1]) transArray.push(arr[i + 1])
+    } else if (arr[i] == '--double-prev') {
+      if (transArray[transArray.length - 1] == arr[i - 1] && arr[i - 1])
+        transArray.push(arr[i - 1])
+    } else transArray.push(arr[i])
+  }
+
+  return transArray
 }
 
 module.exports = {
-  transform
-};
+  transform,
+}
